544979d69b6368c0baf415c7081cbc83f2fd676d
d-figueiredo <diogofilipe@hotmail.fr>
2021-04-29 22:58:09 +0100

    Update base CSS (sizes, font, spacing) and SelectionProcess improvement

--- frontend/src/components/SelectionProcess.js
+++ frontend/src/components/SelectionProcess.js
@@ -1,22 +1,22 @@
 function SelectionProcess() {
   return (
-     <div className="bg-primary text-white text-center">
-          <h3>Processo de seleção</h3>
+     <div className="bg-primary text-white text-center padding-xxl">
+          <h3 className="font-size-l">Processo de seleção</h3>
           <div className="display-flex-center">
             <div className="padding-xl">
-              <p className="card-number">01</p>
-              <p>Dinamicas de Grupo</p>
-              <p>Interação  entre os candidatos focando no trabalho de grupo.</p>
+              <p className="font-size-xxxl font-semi-bold">01</p>
+              <p className="font-size-m font-semi-bold">Dinamicas de Grupo</p>
+              <p className="font-size-s padding-s-xxl">Interação  entre os candidatos focando no trabalho de grupo.</p>
             </div>
             <div className="padding-xl">
-              <p className="card-number">02</p>
-              <p>Entrevista com um Manager</p>
-              <p>Conversa com um Manager da Deloitte sobre as tuas motivações.</p>
+              <p className="font-size-xxxl font-semi-bold">02</p>
+              <p className="font-size-m font-semi-bold">Entrevista com um Manager</p>
+              <p className="font-size-s padding-s-xxl">Conversa com um Manager da Deloitte sobre as tuas motivações.</p>
             </div>
             <div className="padding-xl">
-              <p className="card-number">03</p>
-              <p>Entrevista com um Sócio</p>
-              <p>Conversa com um Sócio da Deloitte sobre a tua pessoa.</p>
+              <p className="font-size-xxxl font-semi-bold">03</p>
+              <p className="font-size-m font-semi-bold">Entrevista com um Sócio</p>
+              <p className="font-size-s padding-s-xxl">Conversa com um Sócio da Deloitte sobre a tua pessoa.</p>
             </div>
           </div>
      </div>
