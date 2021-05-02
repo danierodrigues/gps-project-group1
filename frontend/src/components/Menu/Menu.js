12b70c8c78d9189ffa64c2feab26a0faccc4ca27
d-figueiredo <diogofilipe@hotmail.fr>
2021-05-02 22:35:06 +0100

    Menu improvements and main title

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -2,30 +2,33 @@ import menuItems from './MenuItems';
 import './Menu.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from '../../images/logo-deloitte.png';
-import background from '../../images/background.jpg';
 
 function Menu() {
    return(
-      <div className="bg-test">
-         {/*<div className='bg-wrapper'>
-            <img src={background} className='bg'/>
-         </div>*/}
-         <nav>
-            <input type='checkbox' id='toogle' />
-            <label htmlFor='toogle' className='check-btn'>
-                  <GiHamburgerMenu />
-            </label>
-            <span className='logo-wrapper'>
-               <img alt='Deloitte logo' src={logo} width='170'/>
-            </span>
-            <ul className='font-semi-bold'>
-                  {menuItems.map((item) =>  (
-                        <li key={item.id}>
-                           <a href={item.url}>{item.label}</a>
-                        </li>
-                  ))}
-            </ul>
-         </nav>
+      <div className='bg text-white'>
+         <div>
+            <nav>
+               <input type='checkbox' id='toogle' />
+               <label htmlFor='toogle' className='check-btn'>
+                     <GiHamburgerMenu />
+               </label>
+               <span>
+                  <img alt='Deloitte logo' src={logo} width='170'/>
+               </span>
+               <ul className='font-semi-bold display-inline'>
+                     {menuItems.map((item) =>  (
+                           <li key={item.id}>
+                              <a href={item.url}>{item.label}</a>
+                           </li>
+                     ))}
+               </ul>
+            </nav>
+         </div>
+         <div>
+            <p className='font-size-xxxl font-semi-bold margin-top-xl'>BrightStart</p>
+            <p className='font-size-l font-semi-bold'>O futuro pode ser brilhante</p>
+            <button className='margin-top-xl'>Candidata-te</button>
+         </div>
       </div>
    );
 }
