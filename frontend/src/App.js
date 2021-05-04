d1856a2e3bebce10398eeb00f85164839f3a1018
d-figueiredo <78753465+d-figueiredo@users.noreply.github.com>
2021-05-04 22:38:46 +0100

    Footer added, but the app needs a global improvement (media queries)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import Menu from './components/Menu/Menu';
+import Advantages from './components/Advantages/Advantages';
+import About from './components/About/About';
 import SelectionProcess from './components/SelectionProcess/SelectionProcess';
-import Footer from './components/Footer/footer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <main>
       <Menu />
+      <About />
+      <Advantages />
       <SelectionProcess />
-      <Footer/>
+      <Footer />
     </main>
   );
 }
