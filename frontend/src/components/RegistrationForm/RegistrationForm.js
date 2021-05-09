29162bdd598e8b450f917ef01e8fc56fb8a7fb0d
Daniel <daniel.rodrigues.cavadas@gmail.com>
2021-05-09 17:37:12 +0100

    github actions

--- frontend/src/components/RegistrationForm/RegistrationForm.js
+++ frontend/src/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import universities from '../../Universities';
+import universities from '../../universities';
 import Select from 'react-select';
 import './RegistrationForm.css';
 
