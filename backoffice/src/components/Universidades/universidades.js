d73631e7708b2602a503f771d3f0239750db178f
Silva <paulohesilva@deloitte.pt>
2021-05-17 17:01:40 +0100

    Merged backoffice

---
+++ backoffice/src/components/Universidades/universidades.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './universidades.css';
+
+class Universidades extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Universidades</h1>
+        </div>
+    );
+  }
+}
+
+export default Universidades;
